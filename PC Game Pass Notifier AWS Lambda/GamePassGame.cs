e14cc38e24ecc4dbf5c9144d6b2bb6ef0fc100c2
Dominik Jung <jung.dominik.dj@gmail.com>
2022-06-18 00:08:37 +0200

    Changed embed: footer text -> description

--- PC Game Pass Notifier AWS Lambda/GamePassGame.cs
+++ PC Game Pass Notifier AWS Lambda/GamePassGame.cs
@@ -7,57 +7,53 @@ using Newtonsoft.Json;
 
 namespace PC_Game_Pass_Notifier_AWS_Lambda
 {
-	public class GamePassGame
-	{
-		public string DeveloperName { get; set; }
-		public string ProductTitle { get; set; }
-		public string PublisherName { get; set; }
-		public string ProductDescription { get; set; }
-		public string ShortDescription { get; set; }
-		public string ProductId { get; set; }
-		public string ProductArtUrl { get; set; }
+    public class GamePassGame
+    {
+        public string DeveloperName { get; set; }
+        public string ProductTitle { get; set; }
+        public string PublisherName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ShortDescription { get; set; }
+        public string ProductId { get; set; }
+        public string ProductArtUrl { get; set; }
 
-		public GamePassGame()
-		{
-			DeveloperName = "";
-			ProductTitle = "";
-			PublisherName = "";
-			ProductDescription = "";
-			ShortDescription = "";
-			ProductId = "";
-			ProductArtUrl = "";
-		}
+        public GamePassGame()
+        {
+            DeveloperName = "";
+            ProductTitle = "";
+            PublisherName = "";
+            ProductDescription = "";
+            ShortDescription = "";
+            ProductId = "";
+            ProductArtUrl = "";
+        }
 
-		public string AsUpdateDescription()
-		{
-			StringBuilder stringBuilder = new();
-			return stringBuilder
-				.AppendLine(ProductTitle)
-				.AppendLine(ShortDescription)
-				.Append("Entwickler: ").AppendLine(DeveloperName)
-				.Append("Publisher: ").AppendLine(PublisherName)
-				.ToString();
-		}
+        public string AsUpdateDescription()
+        {
+            StringBuilder stringBuilder = new();
+            return stringBuilder
+                .AppendLine(ProductTitle)
+                .AppendLine(ShortDescription)
+                .Append("Entwickler: ").AppendLine(DeveloperName)
+                .Append("Publisher: ").AppendLine(PublisherName)
+                .ToString();
+        }
 
-		public string AsUpdateEmbed()
-		{
-			Dictionary<string, string> imageObject = new();
-			imageObject.Add("url", ProductArtUrl);
+        public string AsUpdateEmbed()
+        {
+            Dictionary<string, string> imageObject = new();
+            imageObject.Add("url", ProductArtUrl);
 
-			// TODO: Ensure description does not exceed footer text limit of 2048 characters
-			Dictionary<string, string> footerObject = new();
-			footerObject.Add("text", ShortDescription);
+            Dictionary<string, string> embedObject = new();
+            embedObject.Add("url", $"https://www.xbox.com/de-de/games/store/gamepass/{ProductId}");
+            embedObject.Add("title", ProductTitle);
+            embedObject.Add("description", ShortDescription);
+            embedObject.Add("image", JsonConvert.SerializeObject(imageObject));
 
-			Dictionary<string, string> embedObject = new();
-			embedObject.Add("url", $"https://www.xbox.com/de-de/games/store/gamepass/{ProductId}");
-			embedObject.Add("title", ProductTitle);
-			embedObject.Add("image", JsonConvert.SerializeObject(imageObject));
-			embedObject.Add("footer", JsonConvert.SerializeObject(footerObject));
-
-			// Discord embed limit:
-			// Additionally, the combined sum of characters in all title, description, field.name, field.value, footer.text, and author.name fields across all embeds attached
-			// to a message must not exceed 6000 characters. Violating any of these constraints will result in a Bad Request response.
-			return JsonConvert.SerializeObject(embedObject);
-		}
-	}
+            // Discord embed limit:
+            // Additionally, the combined sum of characters in all title, description, field.name, field.value, footer.text, and author.name fields across all embeds attached
+            // to a message must not exceed 6000 characters. Violating any of these constraints will result in a Bad Request response.
+            return JsonConvert.SerializeObject(embedObject);
+        }
+    }
 }
